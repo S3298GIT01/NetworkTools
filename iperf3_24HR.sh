60307bbadd2050696705ff0b586d8b2aafd1f2ca
S3298GIT01 <144739078+S3298GIT01@users.noreply.github.com>
2024-10-20 22:23:42 -0400

    add grep filter

--- iperf3_24HR.sh
+++ iperf3_24HR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    iperf3 -c <server_ip> -t 60 >> iperf_log.txt
+    iperf3 -c <server_ip> -t 60 | grep "0.00-60.00|\0.00-60.01" >> iperf_log.txt
     sleep 3600  # Run every hour
 done
