897257ae6d2d52329e1f9e0a4728f5b19800c7ec
S3298GIT01 <144739078+S3298GIT01@users.noreply.github.com>
2025-09-12 10:36:38 -0400

    Create speedtest_by_interface.sh

---
+++ speedtest_by_interface.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+speedtest --interface ens1 > "test" 2>&1 &
+speedtest --interface ens2 > "test" 2>&1 &
+speedtest --interface ens3 > "test" 2>&1 &
+speedtest --interface ens4 > "test" 2>&1 &
+speedtest --interface ens5 > "test" 2>&1 &
+speedtest --interface ens6 > "test" 2>&1 &
+speedtest --interface ens7 > "test" 2>&1 &
+speedtest --interface ens8 > "test" 2>&1 &
+speedtest --interface ens9 > "test" 2>&1 &
+speedtest --interface ens10 > "test" 2>&1 &
+speedtest --interface ens11 > "test" 2>&1 &
+speedtest --interface ens18 > "test" 2>&1 &
+speedtest --interface ens19 > "test" 2>&1 &
+speedtest --interface ens20 > "test" 2>&1 &
+speedtest --interface ens21 > "test" 2>&1 &
+speedtest --interface ens22 > "test" 2>&1 &
+speedtest --interface ens23 > "test" 2>&1 &
