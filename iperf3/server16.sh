eb54f247b67f7bd9e546c7ddf3572c9c3e49de82
S3298GIT01 <144739078+S3298GIT01@users.noreply.github.com>
2025-09-16 11:31:20 -0400

    Create server16.sh

---
+++ iperf3/server16.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+iperf -s -p 5301 &
+iperf -s -p 5302 &
+iperf -s -p 5303 &
+iperf -s -p 5304 &
+iperf -s -p 5305 &
+iperf -s -p 5306 &
+iperf -s -p 5307 &
+iperf -s -p 5308 &
+iperf -s -p 5309 &
+iperf -s -p 5310 &
+iperf -s -p 5311 &
+iperf -s -p 5312 &
+iperf -s -p 5313 &
+iperf -s -p 5314 &
+iperf -s -p 5315 &
+iperf -s -p 5316 &
+iperf -s -p 5317 &
